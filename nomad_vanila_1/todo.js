2312e7131129a8ae26c19953058af1392aeac9de
DyNamiC <imsong1997@gmail.com>
2020-03-26 17:07:30 +0900

    Update todo.js

--- nomad_vanila_1/todo.js
+++ nomad_vanila_1/todo.js
@@ -9,7 +9,7 @@ function loadTodo(){
 	const loadedTodo = localStorage.getItem(TODO_LS);
 	if(loadedTodo !==null){
 		const parsedTodo = JSON.parse(loadedTodo); //saveTodo에서 string으로 변환된 object를 다시 원래대로 바꿔줌
-		parsedTodo.forEach(function(todo){
+		parsedTodo.forEach(function(todo){ //라인56
 			paintTodo(todo.text);
 		})
 	}
@@ -17,16 +17,16 @@ function loadTodo(){
 
 function paintTodo(text) {
 	const li = document.createElement("li");
-	const btnDelete = document.createElement("button");
+	const btnDelete = document.createElement("button"); //HTML에 버튼태그 추가
 	const newId = todos.length+1;
 	btnDelete.innerText = "X";
-	btnDelete.addEventListener("click", deleteTodo);
-	const span = document.createElement("span");
-	span.innerText = text;
-	li.appendChild(span);
-	li.appendChild(btnDelete);
-	li.id = newId;
-	todoList.appendChild(li);
+	btnDelete.addEventListener("click", deleteTodo); //삭제 기능
+	const span = document.createElement("span"); //HTML에 span태그 추가
+	span.innerText = text; //span태그로 리스트 작성
+	li.appendChild(span); //li태그의 자식요소로 span태그
+	li.appendChild(btnDelete); //li태그의 자식요소로 button태그
+	li.id = newId; //li 삭제를 용이하게 하기 위해 id값을 할당
+	todoList.appendChild(li); //todoList안에 리스트 작성
 
 	const todoObj = {
 		text: text,
@@ -49,10 +49,12 @@ function saveTodo(){ //로컬저장소에 저장하기 위한 함수, string만 
 
 function deleteTodo(){
 	const btn = event.target;
-	const li = btn.parentNode;
-	todoList.removeChild(li);
+	const li = btn.parentNode; //해당 li를 지우기 위해 id값을 변수에 저장
+	todoList.removeChild(li); //해당 id를 가진 li를 삭제
 
-	const cleanTodo = todos.filter(function(todo){ //filter함수&forEach함수 -> 리스트에 있는 모든 item들을 위한 함수(실행)
+	const cleanTodo = todos.filter(function(todo){ 
+		//filter(function) & forEach(function) -> 리스트 또는 배열에 있는 모든 item들을 위한 함수(실행)
+		//filter -> true값인 item만 return
 		return todo.id !== parseInt(li.id);
 	});
 	todos = cleanTodo;
