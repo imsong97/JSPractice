94c6c86562cfc215238025f2bdc7382d2a164262
DyNamiC <imsong1997@gmail.com>
2020-03-26 17:04:19 +0900

    Update greeting.js

--- nomad_vanila_1/greeting.js
+++ nomad_vanila_1/greeting.js
@@ -3,25 +3,26 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser", SHOWING = "showing"; //css에서 제어
 
 function paintGreeting(text) {
-	form.classList.remove(SHOWING);
-	greeting.classList.add(SHOWING);
+	//classList -> add, remove, contains등의 함수를 제어
+	form.classList.remove(SHOWING); //form을 가지고있는 HTML태그에 css의 showing을 적용, form태그 삭제
+	greeting.classList.add(SHOWING); //greeting을 가지고있는 HTML태그에 ~ , h4태그 활성화
 	greeting.innerText = `Hello ${text}`;
 }
 
 function handleSubmit(event){
-	event.preventDefault();
-	const currentValue = input.value;
+	event.preventDefault(); // 엔터키를 누르면 input값이 사라지는 기본속성을 막음
+	const currentValue = input.value; //변수에 input값이 저장
 	paintGreeting(currentValue);
 	saveName(currentValue);
 }
 
 function askName(){
 	form.classList.add(SHOWING);
-	form.addEventListener("submit", handleSubmit);
+	form.addEventListener("submit", handleSubmit); //엔터키를 누르면 이벤트 발생
 }
 
 function saveName(text){
-	localStorage.setItem(USER_LS,text);
+	localStorage.setItem(USER_LS,text); //새로고침을 하면 사라지므로 로컬스토리지에 저장
 }
 
 function loadName(){
